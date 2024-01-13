1e855a1713bd6279d66b131a3e8294701edfaade
Kaju Kopi <kajukopi@gmail.com>
2024-01-13 22:33:45 +0700

    zxc

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const { JWT } = require("google-auth-library");
 
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY,
+  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
