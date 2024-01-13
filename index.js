972d71c7784eece8a1982b742750fe677b97f2d6
Kaju Kopi <kajukopi@gmail.com>
2024-01-14 03:52:20 +0700

    a

--- index.js
+++ index.js
@@ -48,27 +48,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const qrcode = require("qrcode-terminal");
-
-const { Client } = require("whatsapp-web.js");
-const client = new Client();
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", () => {
-  console.log("Client is ready!");
-});
-
-client.on("message", (message) => {
-  if (message.body === "!ping") {
-    message.reply("pong");
-  }
-});
-
-client.initialize();
-
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 const { JWT } = require("google-auth-library");
